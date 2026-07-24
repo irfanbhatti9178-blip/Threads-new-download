7de7b2a66a20fb8cd2d9536469e7e87d824abc2d
irfanbhatti9178-blip <irfanbhatti9178@gmail.com>
2026-07-24 16:51:38 +0500

    Create threads-resolve.js

---
+++ api/threads-resolve.js
@@ -0,0 +1,9 @@
+{
+  "name": "fullytool-threads-resolver",
+  "version": "1.0.0",
+  "description": "Vercel serverless backend for FullyTool's Threads Downloader.",
+  "engines": {
+    "node": ">=18"
+  }
+}
+
